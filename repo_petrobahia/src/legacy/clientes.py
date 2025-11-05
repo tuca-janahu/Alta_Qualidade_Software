11d56d0c7ae1a35831d5fc30b47cf27d746c96a8
tuca-janahu <arturjanahu@gmail.com>
2025-11-05 09:44:43 -0300

    feat: limpar_email

--- repo_petrobahia/src/legacy/clientes.py
+++ repo_petrobahia/src/legacy/clientes.py
@@ -2,14 +2,25 @@ import re
 
 REG_EMAIL = "^[^@\s]+@[^@\s]+\.[^@\s]+$"
 
-def cadastrar_cliente(c):
-    if "email" not in c or "nome" not in c:
+class Cliente:
+    def __init__(self, nome, email):
+        self.nome = nome
+        self.email = email
+        
+def cadastrar_cliente(cliente):
+    if not isinstance(cliente, Cliente):
         print("faltou campo")
         return False
-    if not re.match(REG_EMAIL, c["email"]):
-        print("email invalido mas vou aceitar assim mesmo")
+    
     f = open("clientes.txt", "a", encoding="utf-8")
-    f.write(str(c) + "\n")
+    f.write(str(cliente) + "\n")
     f.close()
-    print("enviando email de boas vindas para", c["email"])
+    print("enviando email de boas vindas para", cliente.email)
     return True
+
+def limpar_email(cliente):
+    if not re.match(REG_EMAIL, cliente.email):
+        print("email invalido mas vou aceitar assim mesmo")
+        
+    cliente.email = cliente.email.replace("@@", "@")
+    return cliente
